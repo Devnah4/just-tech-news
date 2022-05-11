7153f038cd3bef2e0428ce45b37f45b9277f2216
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-10 22:30:10 -0400

    13.2 Completed Code

--- models/user.js
+++ models/user.js
@@ -3,7 +3,12 @@ const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // Checks if the password matches the hashed version
+  checkPassword(loginPW) {
+    return bcrypt.compareSync(loginPW, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -46,9 +51,12 @@ User.init(
         return newUserData;
       },
       async beforeUpdate(updatedUserData) {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
         return updatedUserData;
-      }
+      },
     },
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
