904ccda8a4ad20e4d6325b5d44a2d8aaa3271693
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-11 23:57:40 -0400

    13.3 complete code

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require("./Post");
 
+// creates the association between Post and User
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Post };
