0828605a81e04740b382aa9ef15bda687ea184ca
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-19 18:36:02 -0400

    14.5 complete code
    Added delete and edit functions to logged in users

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
 const { Post, User, Comment, Vote } = require("../../models");
+const withAuth = require('../../utils/auth');
 
 // Pulls all posts from the database
 router.get("/", (req, res) => {
@@ -87,11 +88,11 @@ router.get("/:id", (req, res) => {
 });
 
 // Creates a Post
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
-    user_id: req.body.user_id,
+    user_id: req.session.user_id,
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -114,7 +115,7 @@ router.put("/upvote", (req, res) => {
 });
 
 // Edit title of a post
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -139,7 +140,7 @@ router.put("/:id", (req, res) => {
 });
 
 // Delete a Post
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id,
